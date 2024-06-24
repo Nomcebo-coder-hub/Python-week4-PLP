3e7574bdd35e82f150d2442d92e88060767325a6
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-06-24 12:45:34 +0200

    Create index.py and added class person

---
+++ index.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and I am {self.gender}.")
+
+person_instance = Person("Nomcebo", 26, "female")
+
+person_instance.introduce()
